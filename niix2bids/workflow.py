20189a149d8120a9d84d5c00452c0fcbf85c5588
Benoît Béranger <benoit.beranger@icm-institute.org>
2021-09-27 18:16:21 +0200

    json & nii pairs

--- niix2bids/workflow.py
+++ niix2bids/workflow.py
@@ -55,14 +55,17 @@ def init_logger(out_dir: str, write_file: bool):
 
 ########################################################################################################################
 def fetch_all_files(in_dir: str) -> list:
+
     file_list = []
     for root, dirs, files in os.walk(in_dir):
         for file in files:
             file_list.append(os.path.join(root, file))
+
     if len(file_list)==0:
         log = logging.getLogger(__name__)
         log.error(f"no file found in {in_dir}")
-        sys.exit()
+        sys.exit(1)
+
     file_list.sort()
     return file_list
 
@@ -70,21 +73,33 @@ def fetch_all_files(in_dir: str) -> list:
 ########################################################################################################################
 def isolate_nii_files(in_list: list) -> list:
     log = logging.getLogger(__name__)
+
     r = re.compile(r".*nii$")
     file_list_nii = list(filter(r.match,in_list))
+
     log.info(f"found {len(file_list_nii)} nifti files")
-    if len(file_list_nii)==0:
+    if len(file_list_nii) == 0:
         log.error(f"no .nii file found in {in_list}")
-        sys.exit()
+        sys.exit(1)
+
     return file_list_nii
 
 
 ########################################################################################################################
-def check_if_json_exists(in_list: list) -> None:
+def check_if_json_exists(file_list_nii: list) -> tuple[list, list]:
     log = logging.getLogger(__name__)
-    for file in in_list:
-        if not os.path.exists( os.path.splitext(file)[0] + ".json" ):
+
+    file_list_json = []
+    for file in file_list_nii:
+        jsonfile = os.path.splitext(file)[0] + ".json"
+        if not os.path.exists(jsonfile):
             log.warning(f"this file has no .json associated : {file}")
+            file_list_nii.remove(file)
+        else:
+            file_list_json.append(jsonfile)
+
+    log.info(f"remaining {len(file_list_nii)} nifti files")
+    return file_list_nii, file_list_json
 
 
 ########################################################################################################################
@@ -103,7 +118,7 @@ def run(args: Namespace) -> None:
     # check if input dir exists
     if not os.path.exists(args.nifti_dir):
         log.error(f"nifti_dir does not exist : {args.nifti_dir}")
-        sys.exit()
+        sys.exit(1)
 
     # read all dirs and establish file list
     file_list = fetch_all_files(args.nifti_dir)
@@ -111,5 +126,8 @@ def run(args: Namespace) -> None:
     # isolate .nii files
     file_list_nii = isolate_nii_files(file_list)
 
-    # check if all .nii files have their own
-    check_if_json_exists(file_list_nii)
+    # check if all .nii files have their own .json
+    file_list_nii, file_list_json = check_if_json_exists(file_list_nii)
+
+    # THE END
+    sys.exit(0)
