c64de267747c1f418afb6117db1dc6f8254884c1
Benoît Béranger <benoit.beranger@icm-institute.org>
2021-10-08 18:31:41 +0200

    [FIX / REF]
    - run number is now accurately taken into account
    - huge refactoring to take perform evaluate the run index correctly

--- niix2bids/decision_tree/siemens.py
+++ niix2bids/decision_tree/siemens.py
@@ -14,10 +14,10 @@ log = logging.getLogger(__name__)
 
 ########################################################################################################################
 def mprage(df: pandas.DataFrame, seq_regex: str) -> None:
-    seq_info = utils.slice_with_seqname(df, seq_regex)            # get list of corresponding sequence
-    seq_info = utils.slice_with_mracquistiontype(seq_info, '3D')  # keep 3D images
+    seqinfo = utils.slice_with_seqname(df, seq_regex)            # get list of corresponding sequence
+    seqinfo = utils.slice_with_mracquistiontype(seqinfo, '3D')  # keep 3D images
 
-    if seq_info.empty:  # just run the code faster
+    if seqinfo.empty:  # jus tot run the code faster
         return
 
     # in case of mp2rage, there are 3 (or 4 wih T1map) images generated
@@ -26,169 +26,187 @@ def mprage(df: pandas.DataFrame, seq_regex: str) -> None:
     suffix_regex = ['.*_INV1$', '.*_INV2$', '.*_T1_Images$', '.*_UNI_Images$']
     suffix_list  = ['inv1'    ,  'inv2'   , 'T1map'        , 'UNIT1'         ]
     for suffix in suffix_regex:
-        seq_suffix = utils.slice_with_seriesdescription(seq_info, suffix)
-        idx = 0
-        for row_idx, seq in seq_suffix.iterrows():
-            idx += 1
-            vol = seq['Volume']
-            vol.bidsfields['sub'] = utils.clean_name(seq['PatientName'])
-            vol.bidsfields['ses'] = '01'
-            vol.bidsfields['tag'] = 'anat'
-            vol.bidsfields['acq'] = utils.clean_name(seq['ProtocolName'])
-            vol.bidsfields['run'] = idx
-            vol.bidsfields['suffix'] = suffix_list[suffix_regex.index(suffix)]
-            seq_info = seq_info.drop(row_idx)
+        seq_suffix = utils.slice_with_seriesdescription(seqinfo, suffix)
+        for _, desc_grp in seq_suffix.groupby('SeriesDescription'):
+            run_idx = 0
+            for _, ser_grp in desc_grp.groupby('SeriesNumber'):
+                run_idx += 1
+                for row_idx, seq in ser_grp.iterrows():
+                    vol = seq['Volume']
+                    vol.bidsfields['sub'] = utils.clean_name(seq['PatientName'])
+                    vol.bidsfields['ses'] = '01'
+                    vol.bidsfields['tag'] = 'anat'
+                    vol.bidsfields['acq'] = utils.clean_name(seq['ProtocolName'])
+                    vol.bidsfields['run'] = run_idx
+                    vol.bidsfields['suffix'] = suffix_list[suffix_regex.index(suffix)]
+                    seqinfo = seqinfo.drop(row_idx)
 
     # now that we have dealt with the mp2rage@siemens suffix, we can continue
-    idx = 0
-    for row_idx, seq in seq_info.iterrows():
-        idx += 1
-        vol = seq['Volume']
-        vol.bidsfields['sub'] = utils.clean_name(seq['PatientName'])
-        vol.bidsfields['ses'] = '01'
-        vol.bidsfields['tag'] = 'anat'
-        vol.bidsfields['acq'] = utils.clean_name(seq['ProtocolName'])
-        vol.bidsfields['run'] = idx
-        vol.bidsfields['suffix'] = 'T1w'
+    for _, desc_grp in seqinfo.groupby('SeriesDescription'):
+        run_idx = 0
+        for _, ser_grp in desc_grp.groupby('SeriesNumber'):
+            run_idx += 1
+            for row_idx, seq in ser_grp.iterrows():
+                vol = seq['Volume']
+                vol.bidsfields['sub'] = utils.clean_name(seq['PatientName'])
+                vol.bidsfields['ses'] = '01'
+                vol.bidsfields['tag'] = 'anat'
+                vol.bidsfields['acq'] = utils.clean_name(seq['ProtocolName'])
+                vol.bidsfields['run'] = run_idx
+                vol.bidsfields['suffix'] = 'T1w'
 
 
 ########################################################################################################################
 def tse_vfl(df: pandas.DataFrame, seq_regex: str) -> None:
-    seq_info = utils.slice_with_seqname(df, seq_regex)            # get list of corresponding sequence
-    seq_info = utils.slice_with_mracquistiontype(seq_info, '3D')  # keep 3D images
+    seqinfo = utils.slice_with_seqname(df, seq_regex)            # get list of corresponding sequence
+    seqinfo = utils.slice_with_mracquistiontype(seqinfo, '3D')  # keep 3D images
 
-    if seq_info.empty:  # just run the code faster
+    if seqinfo.empty:  # jus tot run the code faster
         return
 
-    seq_info_T2w   = utils.slice_with_seqvariant(seq_info, '_spc_')
-    seq_info_FLAIR = utils.slice_with_seqvariant(seq_info, '_spcir_')
+    seqinfo_T2w   = utils.slice_with_seqvariant(seqinfo, '_spc_')
+    seqinfo_FLAIR = utils.slice_with_seqvariant(seqinfo, '_spcir_')
 
-    idx = 0
-    for row_idx, seq in seq_info_T2w.iterrows():
-        idx += 1
-        vol = seq['Volume']
-        vol.bidsfields['sub'] = utils.clean_name(seq['PatientName'])
-        vol.bidsfields['ses'] = '01'
-        vol.bidsfields['tag'] = 'anat'
-        vol.bidsfields['acq'] = utils.clean_name(seq['ProtocolName'])
-        vol.bidsfields['run'] = idx
-        vol.bidsfields['suffix'] = 'T2w'
+    for _, desc_grp in seqinfo_T2w.groupby('SeriesDescription'):
+        run_idx = 0
+        for _, ser_grp in desc_grp.groupby('SeriesNumber'):
+            run_idx += 1
+            for row_idx, seq in ser_grp.iterrows():
+                vol = seq['Volume']
+                vol.bidsfields['sub'] = utils.clean_name(seq['PatientName'])
+                vol.bidsfields['ses'] = '01'
+                vol.bidsfields['tag'] = 'anat'
+                vol.bidsfields['acq'] = utils.clean_name(seq['ProtocolName'])
+                vol.bidsfields['run'] = run_idx
+                vol.bidsfields['suffix'] = 'T2w'
 
-    idx = 0
-    for row_idx, seq in seq_info_FLAIR.iterrows():
-        idx += 1
-        vol = seq['Volume']
-        vol.bidsfields['sub'] = utils.clean_name(seq['PatientName'])
-        vol.bidsfields['ses'] = '01'
-        vol.bidsfields['tag'] = 'anat'
-        vol.bidsfields['acq'] = utils.clean_name(seq['ProtocolName'])
-        vol.bidsfields['run'] = idx
-        vol.bidsfields['suffix'] = 'FLAIR'
+    for _, desc_grp in seqinfo_FLAIR.groupby('SeriesDescription'):
+        run_idx = 0
+        for _, ser_grp in desc_grp.groupby('SeriesNumber'):
+            run_idx += 1
+            for row_idx, seq in ser_grp.iterrows():
+                vol = seq['Volume']
+                vol.bidsfields['sub'] = utils.clean_name(seq['PatientName'])
+                vol.bidsfields['ses'] = '01'
+                vol.bidsfields['tag'] = 'anat'
+                vol.bidsfields['acq'] = utils.clean_name(seq['ProtocolName'])
+                vol.bidsfields['run'] = run_idx
+                vol.bidsfields['suffix'] = 'FLAIR'
 
 
 ########################################################################################################################
 def diff(df: pandas.DataFrame, seq_regex: str) -> None:
-    seq_info = utils.slice_with_seqname(df, seq_regex)            # get list of corresponding sequence
-    seq_info = utils.slice_with_mracquistiontype(seq_info, '2D')  # keep 2D images
+    seqinfo = utils.slice_with_seqname(df, seq_regex)            # get list of corresponding sequence
+    seqinfo = utils.slice_with_mracquistiontype(seqinfo, '2D')  # keep 2D images
 
-    if seq_info.empty:  # just run the code faster
+    if seqinfo.empty:  # jus tot run the code faster
         return
 
     # in case of multiband sequence, SBRef images may be generated
     # therefore, we need to deal with them beforehand
-    seq_sbref = utils.slice_with_seriesdescription(seq_info, '.*_SBRef$')
-    idx = 0
-    for row_idx, seq in seq_sbref.iterrows():
-        idx += 1
-        vol = seq['Volume']
-        vol.bidsfields['sub'] = utils.clean_name(seq['PatientName'])
-        vol.bidsfields['ses'] = '01'
-        vol.bidsfields['tag'] = 'dwi'
-        vol.bidsfields['acq'] = utils.clean_name(seq['ProtocolName'])
-        vol.bidsfields['run'] = idx
-        vol.bidsfields['suffix'] = 'sbref'
-        seq_info = seq_info.drop(row_idx)
+    seqinfo_sbref = utils.slice_with_seriesdescription(seqinfo, '.*_SBRef$')
+    for _, desc_grp in seqinfo_sbref.groupby('SeriesDescription'):
+        run_idx = 0
+        for _, ser_grp in desc_grp.groupby('SeriesNumber'):
+            run_idx += 1
+            for row_idx, seq in ser_grp.iterrows():
+                vol = seq['Volume']
+                vol.bidsfields['sub'] = utils.clean_name(seq['PatientName'])
+                vol.bidsfields['ses'] = '01'
+                vol.bidsfields['tag'] = 'dwi'
+                vol.bidsfields['acq'] = utils.clean_name(seq['ProtocolName'])
+                vol.bidsfields['run'] = run_idx
+                vol.bidsfields['suffix'] = 'sbref'
+                seqinfo = seqinfo.drop(row_idx)
 
     # and now the normal volume
-    idx = 0
-    for row_idx, seq in seq_info.iterrows():
-        idx += 1
-        vol = seq['Volume']
-        # check if .bval et .bvec exist
-        vol.check_if_bval_exists()
-        vol.check_if_bvec_exists()
-        vol.bidsfields['sub'] = utils.clean_name(seq['PatientName'])
-        vol.bidsfields['ses'] = '01'
-        vol.bidsfields['tag'] = 'dwi'
-        vol.bidsfields['acq'] = utils.clean_name(seq['ProtocolName'])
-        vol.bidsfields['run'] = idx
-        vol.bidsfields['suffix'] = 'dwi'
+    for _, desc_grp in seqinfo.groupby('SeriesDescription'):
+        run_idx = 0
+        for _, ser_grp in desc_grp.groupby('SeriesNumber'):
+            run_idx += 1
+            for row_idx, seq in ser_grp.iterrows():
+                vol = seq['Volume']
+                # check if .bval et .bvec exist
+                vol.check_if_bval_exists()
+                vol.check_if_bvec_exists()
+                vol.bidsfields['sub'] = utils.clean_name(seq['PatientName'])
+                vol.bidsfields['ses'] = '01'
+                vol.bidsfields['tag'] = 'dwi'
+                vol.bidsfields['acq'] = utils.clean_name(seq['ProtocolName'])
+                vol.bidsfields['run'] = run_idx
+                vol.bidsfields['suffix'] = 'dwi'
 
 
 ########################################################################################################################
 def bold(df: pandas.DataFrame, seq_regex: str) -> None:
-    seq_info = utils.slice_with_seqname(df, seq_regex)            # get list of corresponding sequence
-    seq_info = utils.slice_with_mracquistiontype(seq_info, '2D')  # keep 2D images
+    seqinfo = utils.slice_with_seqname(df, seq_regex)            # get list of corresponding sequence
+    seqinfo = utils.slice_with_mracquistiontype(seqinfo, '2D')  # keep 2D images
 
-    if seq_info.empty:  # just run the code faster
+    if seqinfo.empty:  # jus tot run the code faster
         return
 
     # in case of multiband sequence, SBRef images may be generated
     # therefore, we need to deal with them beforehand
-    seq_sbref = utils.slice_with_seriesdescription(seq_info, '.*_SBRef$')
-    idx = 0
-    for row_idx, seq in seq_sbref.iterrows():
-        idx += 1
-        vol = seq['Volume']
-        vol.bidsfields['sub'] = utils.clean_name(seq['PatientName'])
-        vol.bidsfields['ses'] = '01'
-        vol.bidsfields['tag'] = 'func'
-        vol.bidsfields['acq'] = utils.clean_name(seq['ProtocolName'])
-        vol.bidsfields['run'] = idx
-        if not pandas.isna(seq['EchoNumber']):
-            vol.bidsfields['echo'] = int(seq['EchoNumber'])
-        vol.bidsfields['suffix'] = 'sbref'
-        seq_info = seq_info.drop(row_idx)
+    seqinfo_sbref = utils.slice_with_seriesdescription(seqinfo, '.*_SBRef$')
+    for _, desc_grp in seqinfo_sbref.groupby('SeriesDescription'):
+        run_idx = 0
+        for _, ser_grp in desc_grp.groupby('SeriesNumber'):
+            run_idx += 1
+            for row_idx, seq in ser_grp.iterrows():
+                vol = seq['Volume']
+                vol.bidsfields['sub'] = utils.clean_name(seq['PatientName'])
+                vol.bidsfields['ses'] = '01'
+                vol.bidsfields['tag'] = 'func'
+                vol.bidsfields['acq'] = utils.clean_name(seq['ProtocolName'])
+                vol.bidsfields['run'] = run_idx
+                if not pandas.isna(seq['EchoNumber']):
+                    vol.bidsfields['echo'] = int(seq['EchoNumber'])
+                vol.bidsfields['suffix'] = 'sbref'
+                seqinfo = seqinfo.drop(row_idx)
 
     # separate magnitude & phase images
 
     # magnitude
-    mag = utils.slice_with_imagetype(seq_info,'M')
-    idx = 0
-    for row_idx, seq in mag.iterrows():
-        idx += 1
-        vol = seq['Volume']
-        vol.bidsfields['sub'] = utils.clean_name(seq['PatientName'])
-        vol.bidsfields['ses'] = '01'
-        vol.bidsfields['tag'] = 'func'
-        vol.bidsfields['acq'] = utils.clean_name(seq['ProtocolName'])
-        vol.bidsfields['run'] = idx
-        if not pandas.isna(seq['EchoNumber']):
-            vol.bidsfields['echo'] = int(seq['EchoNumber'])
-        vol.bidsfields['suffix'] = 'bold'
+    seqinfo_mag = utils.slice_with_imagetype(seqinfo,'P')
+    for _, desc_grp in seqinfo_mag.groupby('SeriesDescription'):
+        run_idx = 0
+        for _, ser_grp in desc_grp.groupby('SeriesNumber'):
+            run_idx += 1
+            for row_idx, seq in ser_grp.iterrows():
+                vol = seq['Volume']
+                vol.bidsfields['sub'] = utils.clean_name(seq['PatientName'])
+                vol.bidsfields['ses'] = '01'
+                vol.bidsfields['tag'] = 'func'
+                vol.bidsfields['acq'] = utils.clean_name(seq['ProtocolName'])
+                vol.bidsfields['run'] = run_idx
+                if not pandas.isna(seq['EchoNumber']):
+                    vol.bidsfields['echo'] = int(seq['EchoNumber'])
+                vol.bidsfields['suffix'] = 'bold'
 
     # phase
-    pha = utils.slice_with_imagetype(seq_info,'P')
-    idx = 0
-    for row_idx, seq in pha.iterrows():
-        idx += 1
-        vol = seq['Volume']
-        vol.bidsfields['sub'] = utils.clean_name(seq['PatientName'])
-        vol.bidsfields['ses'] = '01'
-        vol.bidsfields['tag'] = 'func'
-        vol.bidsfields['acq'] = utils.clean_name(seq['ProtocolName'])
-        vol.bidsfields['run'] = idx
-        if not pandas.isna(seq['EchoNumber']):
-            vol.bidsfields['echo'] = int(seq['EchoNumber'])
-        vol.bidsfields['suffix'] = 'pha'
+    seqinfo_pha = utils.slice_with_imagetype(seqinfo,'P')
+    for _, desc_grp in seqinfo_pha.groupby('SeriesDescription'):
+        run_idx = 0
+        for _, ser_grp in desc_grp.groupby('SeriesNumber'):
+            run_idx += 1
+            for row_idx, seq in ser_grp.iterrows():
+                vol = seq['Volume']
+                vol.bidsfields['sub'] = utils.clean_name(seq['PatientName'])
+                vol.bidsfields['ses'] = '01'
+                vol.bidsfields['tag'] = 'func'
+                vol.bidsfields['acq'] = utils.clean_name(seq['ProtocolName'])
+                vol.bidsfields['run'] = run_idx
+                if not pandas.isna(seq['EchoNumber']):
+                    vol.bidsfields['echo'] = int(seq['EchoNumber'])
+                vol.bidsfields['suffix'] = 'pha'
 
 
 ########################################################################################################################
 def fmap(df: pandas.DataFrame, seq_regex: str) -> None:
-    seq_info = utils.slice_with_seqname(df, seq_regex)            # get list of corresponding sequence
-    seq_info = utils.slice_with_mracquistiontype(seq_info, '2D')  # keep 2D images
+    seqinfo = utils.slice_with_seqname(df, seq_regex)            # get list of corresponding sequence
+    seqinfo = utils.slice_with_mracquistiontype(seqinfo, '2D')  # keep 2D images
 
-    if seq_info.empty:  # just run the code faster
+    if seqinfo.empty:  # jus tot run the code faster
         return
 
 
